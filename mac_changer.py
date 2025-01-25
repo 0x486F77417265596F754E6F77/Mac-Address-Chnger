f3b1909d91270ef13e7c30c4e1cc448a047935a0
0x486F77417265596F754E6F77 <ffvffgg02@gmail.com>
2025-01-25 12:25:06 +0100

    Add files via upload
    - This is a simple python script to change your MAC Address on your Linux !

---
+++ mac_changer.py
@@ -0,0 +1,43 @@
+import re
+import subprocess
+import optparse
+
+
+
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface_name", help="-h")
+parser.add_option("-m", "--address", dest="new_mac_address", help="-h")
+options, arguments = parser.parse_args()
+
+if not options.interface_name:
+    parser.error("Usage: python3 macchanger.py -i <Your Interface Here> -m <Choice New Mac Address For Your Interface")
+    exit()
+
+if not options.new_mac_address:
+    parser.error("Usage: python3 macchanger.py -i <Your Interface Here> -m <Choice New Mac Address For Your Interface")
+    exit()
+
+nic_check = subprocess.check_output("ifconfig", shell=True).decode("utf-8")
+
+if str(options.interface_name) in str(nic_check):
+    
+    
+    ifconfig = subprocess.check_output(f"ifconfig {options.interface_name}", shell=True).decode("utf-8")
+    old_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig)[0]
+
+    
+
+    if old_mac != options.new_mac_address and len(options.new_mac_address)==17:
+
+        subprocess.call(f"ifconfig {options.interface_name} down", shell=True)
+        subprocess.call(f"ifconfig {options.interface_name} hw ether {options.new_mac_address}", shell=True)
+        subprocess.call(f"ifconfig {options.interface_name} up", shell=True)
+
+        print(f"[+] Your Mac Address has been changed from {old_mac} to {options.new_mac_address}")
+        print("\n\nCreated By HowAreYouNow!")
+
+    else:
+        print("[-] Use other Mac Address OR Use a correct Mac Address format")
+
+else:
+    print(f"[-] {options.interface_name}: Device not found")
